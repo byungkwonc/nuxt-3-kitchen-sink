4cbaec30afcbb61393d7288ecf4f6152a285390f
최병권 <byungkwonc@gmail.com>
2023-06-12 18:05:22 +0900

    setup

--- pages/private.vue
+++ pages/private.vue
@@ -8,14 +8,11 @@ const username = useState('username');
 <template>
   <div>
     <h1>
-      Welcome <span class="text-primary">{{ username }}</span
-      >!
+      Welcome <span class="text-primary">{{ username }}</span>!
     </h1>
     <p class="mb-6 text-lg">
       You have been given access to the private page 😁🤫
     </p>
-    <NuxtLink to="/middleware" class="p-3 bg-secondary text-white rounded-md"
-      >Back to Home</NuxtLink
-    >
+    <NuxtLink to="/middleware" class="p-3 bg-secondary text-white rounded-md">Back to Home</NuxtLink>
   </div>
 </template>
