45e0d0af55d1d74a2417a781f341baee361244ef
최병권 <byungkwonc@gmail.com>
2023-06-16 17:23:43 +0900

    gtag.js

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import VueGtag from 'vue-gtag';
 
-export const GA_TRACKING_ID = "G-K64QJ0E61J";
-
 Vue.use(VueGtag, {
-    config: { id: GA_TRACKING_ID }
+    config: { id: process.env.google_analytics_id }
   })
